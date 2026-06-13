e16b7abaa5a2ac9e4a770f41c39c0204aae3846d
Manav Arya Singh <144777973+Manavarya09@users.noreply.github.com>
2026-06-13 16:35:36 +0400

    export watermark webhook and savePdf from package entry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export { captureUrl, extractSiteAssets } from './capturer.js';
 export { processScreenshot, optimizePng, toWebp, toAvif, getMetadata } from './optimizer.js';
-export { detectOutputDir, resolveOutputPaths, saveAssets, safeName, nameFromUrl, nameFromComponent } from './output.js';
+export { detectOutputDir, resolveOutputPaths, saveAssets, savePdf, safeName, nameFromUrl, nameFromComponent } from './output.js';
 export { loadConfig, generateConfig } from './config.js';
 export { renderComponent } from './component-renderer.js';
 export { compareScreenshots, createComparisonImage } from './diff.js';
+export { applyWatermark } from './watermark.js';
+export { sendWebhook } from './webhook.js';
